77e9d0c9f35afb2add63770b1179a5b45f8ad762
joseph wang <joseph789222@gmail.com>
2024-06-15 09:38:43 -0400

    Test Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="bg-custom-gray h-screen flex items-center justify-center">
+      <div className = "text-center">
+        <div className="text-custom1 text-white font-Lucon">
+          <p>JOSEPH WANG</p>
+        </div>
+        <div className = "text-2xl text-white font-Lucon 2">
+          <p>hello@joseph.wang</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
